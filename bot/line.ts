cb6756c63e87c0284cd68923ea47d5f232b94e40
MasanoriIwakura <hagobutler@yahoo.co.jp>
2023-02-27 16:43:44 +0900

    Add LINE bot

---
+++ bot/line.ts
@@ -0,0 +1,74 @@
+import { Context } from "https://deno.land/x/hono@v3.0.0/mod.ts";
+import schedules from "../hanage/schedules.ts";
+
+const CHANNEL_ACCESS_TOKEN = Deno.env.get("LINE_CHANNEL_ACCESS_TOKEN") || "";
+
+export default async function (c: Context) {
+  const contentType = c.req.header("Content-Type");
+  if (contentType !== "application/json") {
+    return c.text(`Unsupported Content-Type: ${contentType}`, 415);
+  }
+
+  const json = await c.req.json();
+  if (json.events.length > 0) {
+    const message = json.events[0]?.message?.text;
+    const replyMessage = hanageMessageActions(message);
+    await postReplyMessage(
+      replyMessage,
+      json.events[0]?.replyToken,
+    );
+  }
+
+  return c.text("Success.", 200);
+}
+
+const hanageMessageActions = (text: string | null) => {
+  // TODO: other actions
+  if (text?.includes("今月の鼻毛")) {
+    return monthlyScheduleMessage();
+  } else {
+    return "「今月の鼻毛」とメッセージを送信すると今月のスケジュールを確認できます！";
+  }
+};
+
+const monthlyScheduleMessage = () => {
+  const now = new Date();
+  const thisYear = now.getFullYear();
+  const thisMonth = now.getMonth() + 1;
+
+  const monthlySchedules = schedules[thisYear][thisMonth];
+  let message = "[今月のスケジュール]\n";
+  monthlySchedules.forEach((schedule, _index) => {
+    message = message.concat(
+      `${schedule.from} ~ ${schedule.to} => ${schedule.station.name}\n`,
+    );
+  });
+
+  return message;
+};
+
+const postReplyMessage = async (
+  message: string,
+  replyToken: string,
+) => {
+  const body = {
+    replyToken,
+    messages: [
+      {
+        type: "text",
+        text: message,
+      },
+    ],
+  };
+
+  console.log(body);
+
+  return await fetch("https://api.line.me/v2/bot/message/reply", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${CHANNEL_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify(body),
+  });
+};
--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import v1Station from "./api/v1/station/index.ts";
 import v1Schedule from "./api/v1/schedule/index.ts";
 import v1ScheduleYear from "./api/v1/schedule/year.ts";
 import v1ScheduleMonth from "./api/v1/schedule/month.ts";
+import line from "./bot/line.ts";
 
 const v1 = new Hono();
 v1.get("/stations", (c: Context) => v1Station(c));
@@ -14,5 +15,6 @@ v1.get("/schedules/:year/:month", (c: Context) => v1ScheduleMonth(c));
 const app = new Hono();
 app.route("/v1", v1);
 app.get("/", (c) => c.text("Welcome to Hanage API!!"));
+app.post("/line-bot", (c: Context) => line(c));
 
 serve(app.fetch);
