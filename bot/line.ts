6679acbf32d9dcd636522a43893b781afa00f827
Masanori Iwakura <hagobutler@yahoo.co.jp>
2023-03-24 17:44:31 +0900

    Add may 2023, Change bot result message (#7)
    * feat: add May of 2023
    
    * fix: code format
    
    * feat: change bot result message

--- bot/line.ts
+++ bot/line.ts
@@ -7,22 +7,23 @@ export default async function (c: Context) {
   const json = await c.req.json();
   console.log(json);
 
+  let replyMessage = null;
   if (json.events.length > 0) {
     const message = json.events[0]?.message?.text;
-    const replyMessage = hanageMessageActions(message);
+    replyMessage = hanageMessageActions(message);
 
     if (replyMessage !== null) {
       await postReplyMessage(replyMessage, json.events[0]?.replyToken);
     }
   }
 
-  return c.text("Success.", 200);
+  return c.text(replyMessage ?? "No message", 200);
 }
 
 const hanageMessageActions = (text: string | null) => {
   const now = new Date();
-  const thisYear = now.getFullYear();
-  const thisMonth = now.getMonth() + 1;
+  const thisYear = now.getFullYear().toString();
+  const thisMonth = (now.getMonth() + 1).toString();
 
   if (text?.includes("今月の鼻毛")) {
     return monthlyScheduleMessage(thisYear, thisMonth);
@@ -38,17 +39,18 @@ const hanageMessageActions = (text: string | null) => {
   return null;
 };
 
-const monthlyScheduleMessage = (year, month) => {
+const monthlyScheduleMessage = (year: string, month: string) => {
   const monthlySchedules = schedules[year][month];
 
   const message = `[${month}月の鼻毛]\n`;
   const businessHoursMessage = `[営業時間]\n${businessHours.join("\n")}`;
   const monthlyScheduleMessage = monthlySchedules.reduce(
     (prevValue, schedule) => {
-      const addText = `📅${schedule.from} ~ ${schedule.to}\n🚃${schedule.station.name}\n\n`;
+      const addText =
+        `📅${schedule.from} ~ ${schedule.to}\n🚃${schedule.station.name}\n\n`;
       return prevValue + addText;
     },
-    ""
+    "",
   );
 
   return message + monthlyScheduleMessage + businessHoursMessage;
