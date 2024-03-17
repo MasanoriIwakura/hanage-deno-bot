9f01ebaa89b23ed3029206532ae33f878e200e3c
MasanoriIwakura <iwakura.masa@gmail.com>
2024-03-17 12:00:14 +0900

    Update hono, deno versions

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { Hono } from "https://deno.land/x/hono@v3.0.2/mod.ts";
-export type { Context } from "https://deno.land/x/hono@v3.0.2/mod.ts";
-export { serve } from "https://deno.land/std@0.157.0/http/server.ts";
+export { Hono } from "https://deno.land/x/hono@v4.1.1/mod.ts";
+export type { Context } from "https://deno.land/x/hono@v4.1.1/mod.ts";
+export { serve } from "https://deno.land/std@0.220.1/http/server.ts";
