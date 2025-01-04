c668b405b89984da0ffa52cb8f4d4a4e3b3dcd54
MasanoriIwakura <iwakura.masa@gmail.com>
2025-01-04 16:31:21 +0900

    Fix EKIBANA_URL in screenshot test

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from "@playwright/test";
-import dayjs from "dayjs";
 import Jimp from "jimp";
 
 // TODO: 年で行けるはずだったんだけど、別のリンクになっているので一旦封印
 // const EKIBANA_URL = `https://www.ekibana.com/tokyocalendar${dayjs().year()}`;
 const EKIBANA_URL =
-  "https://www.ekibana.com/%E8%A4%87%E8%A3%BD-%E5%87%BA%E5%BA%97%E3%82%AB%E3%83%AC%E3%83%B3%E3%83%80%E3%83%BC";
+  "https://www.ekibana.com/%E5%87%BA%E5%BA%97%E3%82%AB%E3%83%AC%E3%83%B3%E3%83%80%E3%83%BC";
 
 test("take screenshot", async ({ page }) => {
   await page.goto(EKIBANA_URL);
